5caaaa03d5e4fe51f58b29dc121539c63f2d7177
Osmar <osmarcc70@gmail.com>
2024-10-08 15:25:41 -0600

    Add controllers with method signatures and attributes

---
+++ src/StoreKeeper/OrdersAdmin.java
@@ -0,0 +1,19 @@
+package StoreKeeper;
+
+public class OrdersAdmin {
+
+    public void addOrder(Order order) {
+        // TODO - implement OrdersAdmin.addOrder
+        throw new UnsupportedOperationException();
+    }
+    
+    public void removeOrder(Order order) {
+        // TODO - implement OrdersAdmin.removeOrder
+        throw new UnsupportedOperationException();
+    }
+    
+    public void updateOrder(Order order) {
+        // TODO - implement OrdersAdmin.updateOrder
+        throw new UnsupportedOperationException();
+    }
+}
--- src/model/StoreKeeper/StoreKeeper.java
+++
@@ -1,5 +0,0 @@
-package model.StoreKeeper;
-
-public class StoreKeeper {
-
-}
