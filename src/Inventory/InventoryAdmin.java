243040fb60dae1cf4bbf6b365113ddcb3cba9ad1
Gonzalez Alvarez Maria Fernanda <99632505+mierri@users.noreply.github.com>
2024-10-09 21:26:30 -0600

    Create InventoryAdmin.java

---
+++ src/Inventory/InventoryAdmin.java
@@ -0,0 +1,38 @@
+package Inventory;
+
+import java.util.ArrayList;
+
+public class InventoryAdmin {
+
+    public void addFurniture(PackingList packingList) {
+        ArrayList<Furniture> productsToAdd = packingList.getProducts();
+        if (productsToAdd != null && !productsToAdd.isEmpty()) {
+            // furnitureDatabase.addAll(productsToAdd);
+            System.out.println("Added " + productsToAdd.size() + " items to the inventory.");
+        } else {
+            System.out.println("PackingList is empty or null.");
+        }
+    }
+
+    public void removeFurtinure(PackingList packingList) {
+        ArrayList<Furniture> productsToRemove = packingList.getProducts();
+        if (productsToRemove != null && !productsToRemove.isEmpty()) {
+            // furnitureDatabase.removeAll(productsToRemove);
+            System.out.println("Removed " + productsToRemove.size() + " items from the inventory.");
+        } else {
+            System.out.println("PackingList is empty or null.");
+        }
+    }
+
+    public void updateFurniture(PackingList packingList) {
+        ArrayList<Furniture> productsToUpdate = packingList.getProducts();
+        if (productsToUpdate != null && !productsToUpdate.isEmpty()) {
+            removeFurtinure(packingList);
+            addFurniture(packingList);  
+            System.out.println("Updated inventory with " + productsToUpdate.size() + " items.");
+        } else {
+            System.out.println("PackingList is empty or null.");
+        }
+    }
+
+}
