9b8b684baae363246ab86aac74691b329293a761
Mariano Calderón <125323701+MarianoCalderon@users.noreply.github.com>
2024-10-28 23:33:55 -0600

    Eliminación de validaciones/ cambio prefijos atributos

--- proyecto/MueblesDelgado/src/main/java/Proyect/Logistics/TruckDriver.java
+++ proyecto/MueblesDelgado/src/main/java/Proyect/Logistics/TruckDriver.java
@@ -1,8 +1,11 @@
 package Proyect.Logistics;
 
 public class TruckDriver {
-    private String a_name = null;
-    private int a_licenseNumber = 0;
+    private String name = null;
+    private int licenseNumber = 0;
+    
+    TruckDriverValidationUtils truckDriverValidator = new TruckDriverValidationUtils();
+
 
     public TruckDriver(String p_name, int p_licenseNumber){
         setName(p_name);
@@ -10,48 +13,27 @@ public class TruckDriver {
     }
 
     private void setName(String p_name) {
-        boolean nameIsValid = validateName(p_name);
+        boolean nameIsValid = truckDriverValidator.validateName(p_name);
         if(nameIsValid){
-            this.a_name = p_name;
+            this.name = p_name;
         }
     }
 
-    private boolean validateName(String p_name){
-        boolean isValid = false;
-        if(p_name != null){
-            isValid = true;
-        }else{
-            throw new IllegalArgumentException("NAME REQUIRED");
-        }
-        return isValid;
-    }
 
     private void setLicenseNumber(int p_licenseNumber){
-        boolean licenseNumberIsValid = validateLicenseNumber(p_licenseNumber);
+        boolean licenseNumberIsValid = truckDriverValidator.validateLicenseNumber(p_licenseNumber);
         if(licenseNumberIsValid){
-            this.a_licenseNumber = p_licenseNumber;
+            this.licenseNumber = p_licenseNumber;
         }
 
-    }
-
-    private boolean validateLicenseNumber(int p_licenseNumber){
-        int minimumValue = 0;
-        boolean isValid = false;
-
-        if(p_licenseNumber > minimumValue){
-            isValid = true;
-        }else{
-            throw new IllegalArgumentException("LICENSE NUMBER MUST BE GREATER THAN 0");
-        }
-        return isValid;
     }
 
     public String getName() {
-        return a_name;
+        return name;
     }
 
     public int getLicenseNumber() {
-        return a_licenseNumber;
+        return licenseNumber;
     }
 
     @Override
