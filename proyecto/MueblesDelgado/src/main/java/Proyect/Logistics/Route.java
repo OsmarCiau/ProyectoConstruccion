128d7013980b5b75cc2946a6ba64717ea0fb79ec
Mariano Calderón <125323701+MarianoCalderon@users.noreply.github.com>
2024-10-28 23:26:41 -0600

    Eliminación de validaciones/ cambio prefijos atributos

--- proyecto/MueblesDelgado/src/main/java/Proyect/Logistics/Route.java
+++ proyecto/MueblesDelgado/src/main/java/Proyect/Logistics/Route.java
@@ -3,11 +3,14 @@ package Proyect.Logistics;
 import java.time.LocalTime;
 
 public class Route {
-    private int a_routeId = 0;
-    private String a_originLocation = null;
-    private String a_destination = null;
-    private float a_distance = 0.0f;
-    private LocalTime a_estimatedTime = LocalTime.of(0, 0);
+    private int routeId = 0;
+    private String originLocation = null;
+    private String destination = null;
+    private float distance = 0.0f;
+    private LocalTime estimatedTime = LocalTime.of(0, 0);
+
+    RouteValidationUtils routeValidator = new RouteValidationUtils();
+    
 
     public Route(int p_routeId, String p_originLocation, String p_destination,
                  float p_distance, LocalTime p_estimatedTime){
@@ -19,110 +22,65 @@ public class Route {
     }
 
     private void setRouteId(int p_routeId){
-        boolean routeIdIsValid = validateRouteId(p_routeId);
+        boolean routeIdIsValid = routeValidator.validateRouteId(p_routeId);
         if(routeIdIsValid){
-            this.a_routeId = p_routeId;
+            this.routeId = p_routeId;
         }
     }
 
-    private boolean validateRouteId(int p_routeId){
-        boolean isValid = false;
-        int minimumValue= 0;
-
-        if(p_routeId > minimumValue){
-            isValid = true;
-        }else{
-            throw new IllegalArgumentException("ID MUST BE GREATER THAN 0");
-        }
-
-        return isValid;
-    }
 
     private void setOriginLocation(String p_originLocation){
-        boolean originLocationIsValid = validateOriginLocation(p_originLocation);
+        boolean originLocationIsValid = routeValidator.validateOriginLocation(p_originLocation);
         if(originLocationIsValid){
-            this.a_originLocation = p_originLocation;
+            this.originLocation = p_originLocation;
         }
 
     }
 
-    private boolean validateOriginLocation(String p_originLocation){
-        boolean isValid = false;
-        if(p_originLocation != null){
-            isValid = true;
-        }else{
-            throw new IllegalArgumentException("ORIGIN LOCATION REQUIRED");
-        }
-        return isValid;
-    }
 
     private void setDestination(String p_destination) {
-        boolean destinationIsValid = validateDestination(p_destination);
+        boolean destinationIsValid =  routeValidator.validateDestination(p_destination);
         if(destinationIsValid){
-            this.a_destination = p_destination;
+            this.destination = p_destination;
         }
     }
 
-    private boolean validateDestination(String p_destination){
-        boolean isValid = false;
-        if(p_destination != null){
-            isValid = true;
-        }else{
-            throw new IllegalArgumentException("DESTINATION REQUIRED");
+    private void setDistance(float p_distance) {
+        boolean distanceIsValid = routeValidator.validateDistance(p_distance);
+        if(distanceIsValid){
+            this.distance = p_distance;
         }
-
-        return isValid;
 
     }
 
-    private void setDistance(float distance) {
-        float minimumDistinace = 0.0f;
 
-        if(distance > minimumDistinace){
-            this.a_distance = distance;
-        }else{
-            throw new IllegalArgumentException("DISTANCE MUST BE GREATER THAN 0.0 KM");
-        }
-    }
 
     private void setEstimatedTime(LocalTime p_estimatedTime){
-        boolean estimatedTimeIsValid = validateEstimatedTime(p_estimatedTime);
+        boolean estimatedTimeIsValid = routeValidator.validateEstimatedTime(p_estimatedTime);
         if(estimatedTimeIsValid){
-            this.a_estimatedTime = p_estimatedTime;
+            this.estimatedTime = p_estimatedTime;
         }
     }
 
-    private boolean validateEstimatedTime(LocalTime p_estimatedTime){
-        boolean isValid = false;
-        LocalTime minimumTime = LocalTime.of(0, 0);
-
-        if(p_estimatedTime.equals(minimumTime)){
-            throw new IllegalArgumentException("ESTIMATED TIME MUST BE LONGER THAN 0:00");
-        }else{
-            isValid = true;
-        }
-
-        return isValid;
-    }
 
     public int getIdRoute() {
-        return a_routeId;
+        return routeId;
     }
 
     public String getOriginLocation() {
-        return a_originLocation;
+        return originLocation;
     }
 
     public String getDestination() {
-        return a_destination;
+        return destination;
     }
 
     public float getDistance() {
-        return a_distance;
+        return distance;
     }
 
     public LocalTime getEstimatedTime() {
-        return a_estimatedTime;
+        return estimatedTime;
     }
 
     @Override
