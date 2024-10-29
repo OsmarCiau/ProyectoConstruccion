90a59e95dcfcf5780c752339d9635b3504a1dc8c
Mariano Calderón <125323701+MarianoCalderon@users.noreply.github.com>
2024-10-29 12:12:49 -0600

    Refactorización Validaciones (métodos void)

--- proyecto/MueblesDelgado/src/main/java/Proyect/Logistics/TruckAssignmentValidationUtils.java
+++ proyecto/MueblesDelgado/src/main/java/Proyect/Logistics/TruckAssignmentValidationUtils.java
@@ -1,20 +1,17 @@
 package Proyect.Logistics;
 
 public class TruckAssignmentValidationUtils {
-
-    public boolean validateDeliveryTruck(DeliveryTruck p_deliveryTruck) {
+    public void validateDeliveryTruck(DeliveryTruck p_deliveryTruck) {
         if (p_deliveryTruck == null) {
             throw new IllegalArgumentException("DELIVERY TRUCK REQUIRED");
         }
-
-        return true; // Si no lanza excepción, el camión es válido
     }
 
-    public boolean validateTruckDriver(TruckDriver p_truckDriver) {
+    public void validateTruckDriver(TruckDriver p_truckDriver) {
         if (p_truckDriver == null) {
             throw new IllegalArgumentException("TRUCK DRIVER REQUIRED");
         }
-        return true; // Si no lanza excepción, el conductor es válido
     }
+ 
 
 }
