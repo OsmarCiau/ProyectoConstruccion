2cfeb69df9f39863cd6f5a64d3cedcb95e9ea04b
Mariano Calderón <125323701+MarianoCalderon@users.noreply.github.com>
2024-10-28 23:30:34 -0600

    Eliminación de validaciones/ cambio prefijos atributos

--- proyecto/MueblesDelgado/src/main/java/Proyect/Logistics/RoutePlanner.java
+++ proyecto/MueblesDelgado/src/main/java/Proyect/Logistics/RoutePlanner.java
@@ -6,59 +6,39 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 
 public class RoutePlanner {
-    private String a_warehouseLocation = null;
-    private LocalTime a_startTime = LocalTime.of(0, 0);
+    private String warehouseLocation = null;
+    private LocalTime startTime = LocalTime.of(0, 0);
+
+    RoutePlannerValidationUtils routePlannerValidator = new RoutePlannerValidationUtils();
+
 
     public RoutePlanner(String p_wareHouseLocation, LocalTime p_startTime){
         setWarehouseLocation(p_wareHouseLocation);
-
-
+        setStartTime(p_startTime);
     }
 
     private void setWarehouseLocation(String p_warehouseLocation) {
-        boolean warehouseLocationIsValid = validateWarehouseLocation(p_warehouseLocation);
+        boolean warehouseLocationIsValid = routePlannerValidator.validateWarehouseLocation(p_warehouseLocation);
         if(warehouseLocationIsValid){
-            this.a_warehouseLocation = p_warehouseLocation;
+            this.warehouseLocation = p_warehouseLocation;
         }
     }
 
-    private boolean validateWarehouseLocation(String p_warehouseLocation){
-        boolean isValid = false;
-        if(p_warehouseLocation != null){
-            isValid = true;
-        }else{
-            throw new IllegalArgumentException("WAREHOUSE LOCATION REQUIRED");
-        }
-        return isValid;
 
-    }
     private void setStartTime(LocalTime p_startTime) {
-        boolean startTimeIsValid = validateStartTime(p_startTime);
+        boolean startTimeIsValid = routePlannerValidator.validateStartTime(p_startTime);
         if(startTimeIsValid){
-            this.a_startTime = p_startTime;
+            this.startTime = p_startTime;
         }
 
-    }
-
-    private boolean validateStartTime(LocalTime p_startTime){
-        boolean isValid = false;
-        LocalTime minimumTime = LocalTime.of(8,0);
-
-        if(p_startTime.isAfter(minimumTime)){
-            isValid = true;
-        }else{
-            throw new IllegalArgumentException("START TIME MUST BE AFTER 8:00 AM");
-        }
-
-        return isValid;
     }
 
     public String getWarehouseLocation() {
-        return a_warehouseLocation;
+        return warehouseLocation;
     }
 
     public LocalTime getStartTime() {
-        return a_startTime;
+        return startTime;
     }
 
     public LocalTime estimateDeliveryTimeForOrder(Order p_order){
