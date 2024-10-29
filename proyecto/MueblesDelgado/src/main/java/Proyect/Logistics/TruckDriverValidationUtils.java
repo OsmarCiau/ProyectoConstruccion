0173ef5b4924324d14b98189870fdc336ebaaa45
Mariano Calderón <125323701+MarianoCalderon@users.noreply.github.com>
2024-10-29 12:17:00 -0600

    Refactorización Validaciones (métodos void)

--- proyecto/MueblesDelgado/src/main/java/Proyect/Logistics/TruckDriverValidationUtils.java
+++ proyecto/MueblesDelgado/src/main/java/Proyect/Logistics/TruckDriverValidationUtils.java
@@ -1,25 +1,18 @@
 package Proyect.Logistics;
 
 public class TruckDriverValidationUtils {
-    public boolean validateName(String p_name){
-        boolean isValid = false;
-        if(p_name != null){
-            isValid = true;
-        }else{
+    public void validateName(String p_name){
+        if(p_name == null){
             throw new IllegalArgumentException("NAME REQUIRED");
         }
-        return isValid;
     }
 
-    public boolean validateLicenseNumber(int p_licenseNumber){
+    public void validateLicenseNumber(int p_licenseNumber){
         int minimumValue = 0;
-        boolean isValid = false;
 
-        if(p_licenseNumber > minimumValue){
-            isValid = true;
-        }else{
+        if(p_licenseNumber <= minimumValue){
             throw new IllegalArgumentException("LICENSE NUMBER MUST BE GREATER THAN 0");
         }
-        return isValid;
-    }
+    } 
+    
 }
