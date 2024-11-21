14767006ac829813e73d454a260d970c73ad9e1b
Osmar <osmarcc70@gmail.com>
2024-11-21 04:27:56 -0600

    cambio ruta auth

--- proyecto/MueblesDelgado/src/main/java/Proyect/Controllers/AdministratorAuthContoller.java
+++ proyecto/MueblesDelgado/src/main/java/Proyect/Controllers/AdministratorAuthContoller.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/authentication")
+@RequestMapping("/api/auth")
 public class AdministratorAuthContoller {
 
     @Autowired
