6a1294f91290aba42099f9b98c94515954d9ed1d
Osmar <osmarcc70@gmail.com>
2024-11-22 03:12:21 -0600

    quitar comentarios

--- proyecto/MueblesDelgado/src/main/java/Proyect/MueblesDelgadoApplication.java
+++ proyecto/MueblesDelgado/src/main/java/Proyect/MueblesDelgadoApplication.java
@@ -11,18 +11,4 @@ public class MueblesDelgadoApplication {
     public static void main(String[] args) {
         SpringApplication.run(MueblesDelgadoApplication.class, args);
     }
-
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**")
-//                        .allowedOrigins("http://localhost:5173")
-//                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                        .allowedHeaders("*")
-//                        .allowCredentials(true);
-//            }
-//        };
-//    }
 }
